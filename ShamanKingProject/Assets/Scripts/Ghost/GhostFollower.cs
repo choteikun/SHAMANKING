92534bad433d86bf56b2c86ba90f9e04671bf81d
darkcatneko <darkcat7126@gmail.com>
2023-09-08 18:00:25 +0800

    add aiming point

--- ShamanKingProject/Assets/Scripts/Ghost/GhostFollower.cs
+++ ShamanKingProject/Assets/Scripts/Ghost/GhostFollower.cs
@@ -1,11 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using Language.Lua;
+using System;
+using Gamemanager;
 
 public class GhostFollower : MonoBehaviour
 {
     [SerializeField]
     Transform target_; // 物體B
+    [SerializeField]
+    Transform aimingGhostPoint_;
     [SerializeField]
     float positionSmoothSpeed_ = 0.5f;
     [SerializeField]
@@ -15,7 +19,17 @@ public class GhostFollower : MonoBehaviour
 
     void Start()
     {
-        positionOffset_ = transform.position - target_.position;
+        setOffSet(target_);
+        //GameManager.Instance.MainGameEvent.SetSubscribe(GameManager.Instance.MainGameEvent.OnAimingButtonTrigger,x=> setOffSet(aimingGhostPoint_));
+        //var onAimingEnterEvent = GameManager.Instance.MainGameEvent.OnAimingButtonTrigger.Where(cmd => cmd.AimingButtonIsPressed).Subscribe(cmd => setOffSet(aimingGhostPoint_));
+        //GameManager.Instance.MainGameMediator.AddToDisposables(onAimingEnterEvent);
+
+
+    }
+
+    void setOffSet(Transform target)
+    {
+        positionOffset_ = transform.position - target.position;
         rotationOffset_ = Quaternion.identity; // 初始時沒有旋轉偏移
     }
 
