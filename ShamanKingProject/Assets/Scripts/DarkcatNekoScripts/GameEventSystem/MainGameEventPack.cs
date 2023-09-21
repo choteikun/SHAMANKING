4ae2ad13d18dec2e8b55b308f0adf6f65986981f
darkcatneko <darkcat7126@gmail.com>
2023-09-21 17:44:20 +0800

    加入注釋

--- ShamanKingProject/Assets/Scripts/DarkcatNekoScripts/GameEventSystem/MainGameEventPack.cs
+++ ShamanKingProject/Assets/Scripts/DarkcatNekoScripts/GameEventSystem/MainGameEventPack.cs
@@ -9,29 +9,68 @@ public class MainGameEventPack : GameEventPack
 {
     public IObservable<TestInputCommand> OnTestInput => getSubject<TestInputCommand>();
 
+    /// <summary>
+    /// 手把輸入的角色移動
+    /// </summary>
     public IObservable<PlayerControllerMovementCommand> OnPlayerControllerMovement => getSubject<PlayerControllerMovementCommand>();
 
+    /// <summary>
+    /// 手把輸入的角色鏡頭移動
+    /// </summary>
     public IObservable<PlayerControllerCameraRotateCommand> OnPlayerCameraRotate => getSubject<PlayerControllerCameraRotateCommand>();
 
+    /// <summary>
+    /// 按下與放開瞄準鏡的瞬間
+    /// </summary>
     public IObservable<PlayerAimingButtonCommand> OnAimingButtonTrigger => getSubject<PlayerAimingButtonCommand>();
 
+    /// <summary>
+    /// 手把輸入角色翻滾
+    /// </summary>
     public IObservable<PlayerRollingButtonCommand> OnPlayerRoll => getSubject<PlayerRollingButtonCommand>();
 
+    /// <summary>
+    /// 手把輸入發射鍵
+    /// </summary>
     public IObservable<PlayerLaunchGhostButtonCommand> OnPlayerLaunchGhost => getSubject<PlayerLaunchGhostButtonCommand>();
 
+    /// <summary>
+    /// 角色的發射到達終點，回傳角色是否撞到東西，與角色撞到的東西
+    /// </summary>
     public IObservable<PlayerLaunchActionFinishCommand> OnPlayerLaunchActionFinish => getSubject<PlayerLaunchActionFinishCommand>();
 
+    /// <summary>
+    /// 手把輸入角色跳躍
+    /// </summary>
     public IObservable<PlayerJumpButtonCommand> OnPlayerJump => getSubject<PlayerJumpButtonCommand>();
 
+    /// <summary>
+    /// 手把輸入角色輕攻擊鍵
+    /// </summary>
     public IObservable<PlayerLightAttackButtonCommand> OnPlayerLightAttack => getSubject<PlayerLightAttackButtonCommand>();
 
+    /// <summary>
+    /// 幽靈發出的動畫事件
+    /// </summary>
     public IObservable<GhostAnimationEventsCommand> OnGhostAnimationEvents => getSubject<GhostAnimationEventsCommand>();
 
+    /// <summary>
+    /// 手把輸入角色取消附身
+    /// </summary>
     public IObservable<PlayerCancelPossessCommand> OnPlayerCancelPossess => getSubject<PlayerCancelPossessCommand>();
 
+    /// <summary>
+    /// 少女發出的動畫事件
+    /// </summary>
     public IObservable<PlayerAnimationEventsCommand> OnPlayerAnimationEvents => getSubject<PlayerAnimationEventsCommand>();
 
+    /// <summary>
+    /// 幽靈的發射事件完整結束時發出的信號
+    /// </summary>
     public IObservable<GhostLaunchProcessFinishResponse> OnGhostLaunchProcessFinish => getSubject<GhostLaunchProcessFinishResponse>();
 
+    /// <summary>
+    /// 角色不可移動狀態結束指令
+    /// </summary>
     public IObservable<PlayerMovementInterruptionFinishCommand> OnPlayerMovementInterruptionFinish => getSubject<PlayerMovementInterruptionFinishCommand>();
 }
