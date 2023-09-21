f57539ab8bb9e352840a06f02ce620f600d1834e
darkcatneko <darkcat7126@gmail.com>
2023-09-21 15:43:58 +0800

    merge

---
+++ ShamanKingProject/Assets/Scripts/Tool/DataManager/DataManager.cs
@@ -0,0 +1,91 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Datamanager
+{
+    public class DataManager
+    {
+        public DataGroup DataGroup = new DataGroup();
+        public async Task InitDataMananger()
+        {
+            var CSVString = await AddressableSearcher.GetAddressableAssetAsync<TextAsset>("CSV/BoomerCSV");
+            var stringData = await CSVClassGenerator.GenClassArrayByCSV<DatasPath>(CSVString);
+            PropertyInfo[] propertyInfo = typeof(DataGroup).GetProperties();
+            for (int i = 0; i < propertyInfo.Length; i++)
+            {
+                var dataObj = propertyInfo[i].GetValue(DataGroup);
+                Type type;
+                if (dataObj is IDataBase database)
+                {
+                    type = database.ThisDataType;
+                    var CSVstring = await AddressableSearcher.GetAddressableAssetAsync<TextAsset>(stringData[i].Path);
+                    var List = await CSVClassGenerator.GenClassArrayByCSV(type, CSVstring);
+                    database.DataArray = List;
+                }
+            }
+            await UniTask.Delay(100);
+            //GameManager.Instance.GameFinishInit = true;
+            //
+        }
+        public T GetDataByID<T>(int id) where T : class
+        {
+            var database = DataGroup.TryGetDataBase<T>();
+            foreach (var item in database.DataArray)
+            {
+                if (item is not IWithIdData data)
+                {
+                    throw new UnityException("Not a IWithIdData");
+                }
+                if (data.Id == id)
+                {
+                    return data as T;
+                }
+            }
+            return null;
+        }
+        public T GetDataByName<T>(string name) where T : class
+        {
+            var database = DataGroup.TryGetDataBase<T>();
+            foreach (var item in database.DataArray)
+            {
+                if (item is not IWithNameData data)
+                {
+                    throw new UnityException("Not a IWithNameData");
+                }
+                if (data.Name == name)
+                {
+                    return data as T;
+                }
+            }
+            return null;
+        }
+        //public float GetSpeedPercentageByHeldingCoin(int coinCount)
+        //{
+        //    for (int i = 0; i < DataGroup.GuardianSpeedPercentageDataBase.DataArray.Length; i++)
+        //    {
+        //        var templete = GetDataByID<GuardianSpeedPercentageByCoinThresholdTemplete>(i);
+        //        if (coinCount >= templete.CoinThreshold)
+        //        {
+        //            return templete.SpeedRatio;
+        //        }
+        //    }
+        //    return 1f;
+        //}
+        //public float GetDropedCoinPercentageByHeldingCoin(int coinCount)
+        //{
+        //    for (int i = 0; i < DataGroup.PlayerDropedCoinPercentageDataBase.DataArray.Length; i++)
+        //    {
+        //        var templete = GetDataByID<PlayerDropedCoinPercentageByCoinThresholdTemplete>(i);
+        //        if (coinCount >= templete.CoinThreshold)
+        //        {
+        //            return templete.DropedRatio;
+        //        }
+        //    }
+        //    return 1f;
+        //}
+    }
+}
+
