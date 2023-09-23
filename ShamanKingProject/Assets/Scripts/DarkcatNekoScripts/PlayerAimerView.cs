1b33af16c0aa6f8bf38e7bb3c4f3d25f460fc703
darkcatneko <darkcat7126@gmail.com>
2023-09-23 14:56:05 +0800

    ADD_RayCubeView

--- ShamanKingProject/Assets/Scripts/DarkcatNekoScripts/PlayerAimerView.cs
+++ ShamanKingProject/Assets/Scripts/DarkcatNekoScripts/PlayerAimerView.cs
@@ -11,18 +11,37 @@ public class PlayerAimerView : MonoBehaviour
     float distance_;
     [SerializeField]
     float smoothing_;
+    [SerializeField]
+    GameObject rayCube_;
+    [SerializeField]
+    GameObject aimStartPoint_;
+    [SerializeField]
+    GameObject[] objectsInArea_;
     private void Update()
     {
         var screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
-        if (Physics.Raycast(ray, out RaycastHit hit, distance_, aimColloderLayerMask))
-        {
-            aimPoint_.transform.position = Vector3.Lerp(aimPoint_.transform.position, hit.point, smoothing_ * Time.deltaTime);
-        }
-        else
-        {
+        //if (Physics.Raycast(ray, out RaycastHit hit, distance_, aimColloderLayerMask))
+        //{
+        //    aimPoint_.transform.position = Vector3.Lerp(aimPoint_.transform.position, hit.point, smoothing_ * Time.deltaTime);
+        //}
+        //else
+        //{
             var endPoint = ray.origin + ray.direction * distance_;
             aimPoint_.transform.position = Vector3.Lerp(aimPoint_.transform.position, endPoint, smoothing_ * Time.deltaTime);
-        }
+            rayCubeUpdate(aimPoint_.transform.position);
+        //}
+    }
+
+    void rayCubeUpdate(Vector3 pointB)
+    {
+        var pointA = Camera.main.transform.position;
+        rayCube_.transform.localScale = new Vector3(0.5f, 0.5f, distance_);
+        rayCube_.transform.position = (pointA + pointB) / 2f;
+        Vector3 directionToB = (pointB - pointA).normalized;
+        Quaternion rotation = Quaternion.LookRotation(directionToB);
+
+        // 应用旋转角度
+        rayCube_.transform.rotation = rotation;
     }
 }
