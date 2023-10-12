4acf5a129696ab9e834b368d190c2a33b13a5628
darkcatneko <darkcat7126@gmail.com>
2023-10-12 18:05:49 +0800

    add sizeChanger

---
+++ ShamanKingProject/Assets/Scripts/Ghost/GhostSizeController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSizeController : MonoBehaviour
+{
+    [SerializeField] GameObject ghostPrefab_;
+    void Start()
+    {
+        GameManager.Instance.MainGameEvent.SetSubscribe(GameManager.Instance.UIGameEvent.OnSpiritUpdate, cmd => { updateGhostSize(); });
+    }
+
+    void updateGhostSize()
+    {
+        ghostPrefab_.transform.localScale = new Vector3(1, 1, 1) * (1 + 0.25f * GameManager.Instance.MainGameMediator.RealTimePlayerData.GhostNowEatAmount);
+    }
+}
--- ShamanKingProject/Assets/Test/HitableItemTest.cs
+++ ShamanKingProject/Assets/Test/HitableItemTest.cs
@@ -1,10 +1,16 @@
 using Gamemanager;
 using System.Collections;
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
 public class HitableItemTest : MonoBehaviour
 {
     [field:SerializeField] public GameObject onHitPoint_ { get; private set; }
     [field: SerializeField] public HitObjecctTag HitTag { get; private set; }
+
+    private void Start()
+    {
+        GameManager.Instance.MainGameEvent.OnPlayerBiteFinish.Where(cmd => cmd.HitObject == this.gameObject).Subscribe(cmd => { this.gameObject.SetActive(false); });
+    }
 }
