6b79dffa7e2336329d21516ee502a7fc0c829602
草蛇 <blackzxz77@gmail.com>
2023-10-19 14:01:47 +0800

    敵人瞬移功能研究

--- ShamanKingProject/Assets/Scripts/GrassSnakeScripts/Snake_BehaviorTrees/EnemyDashAttack.cs
+++ ShamanKingProject/Assets/Scripts/GrassSnakeScripts/Snake_BehaviorTrees/EnemyDashAttack.cs
@@ -1,18 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime.Tasks;
+using Cysharp.Threading.Tasks;
+using TooltipAttribute = BehaviorDesigner.Runtime.Tasks.TooltipAttribute;
 
-public class EnemyDashAttack : MonoBehaviour
+[TaskCategory("Snake")]
+[TaskDescription("敵人衝刺功能")]
+public class EnemyDashAttack : Action
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("衝刺時間")]
+    public float DashTime;
+    [Tooltip("衝刺速度")]
+    public float DashSpeed;
+    [Tooltip("Should the value be reverted back to its original value after it has been set?")]
+    public bool setOnce;
+
+    bool isDashing;
+
+
+    public override TaskStatus OnUpdate()
     {
+        if (setOnce)
+        {
+            Dash(transform.forward);
+        }
+        return TaskStatus.Success;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    //衝刺測試
+    async void Dash(Vector3 targetDir)
     {
-        
+        if (isDashing) return; // 如果已經在衝刺中，則直接返回
+
+        isDashing = true;
+        float startTime = Time.time;
+        Vector3 initialPosition = transform.position;
+
+        while (Time.time < startTime + DashTime)
+        {
+            float elapsedTime = Time.time - startTime;
+            float dashDistance = elapsedTime * DashSpeed;
+
+            transform.position = initialPosition + (targetDir.normalized * dashDistance);
+            await UniTask.Yield();
+        }
+        isDashing = false;
     }
 }
