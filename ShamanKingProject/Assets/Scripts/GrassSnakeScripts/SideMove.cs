998a5a7e2aa4077bba2e86899bf315271780886d
choteikun <blackzxz77@gmail.com>
2023-11-09 18:22:28 +0800

    往左往右，我出去了又跳回來了，诶嘿你抓不到我---的測試

---
+++ ShamanKingProject/Assets/Scripts/GrassSnakeScripts/SideMove.cs
@@ -0,0 +1,49 @@
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+using TooltipAttribute = BehaviorDesigner.Runtime.Tasks.TooltipAttribute;
+
+[TaskCategory("Snake")]
+public class SideMove : Action
+{
+    [Tooltip("The speed of the agent")]
+    public SharedFloat speed;
+    [Tooltip("Should the agent be looking at the target position?")]
+    public SharedBool lookAtTarget = true;
+    [Tooltip("Max rotation delta if lookAtTarget is enabled")]
+    public SharedFloat maxLookAtRotationDelta;
+    [Tooltip("面向目標")]
+    public SharedGameObject targetObj;
+    [Tooltip("side Pos Value")]
+    public Vector3 SidePosValue;
+
+    public override TaskStatus OnUpdate()
+    {
+        var position = Target();
+
+        //面向目標並進行移動
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + SidePosValue, speed.Value * Time.deltaTime);
+        if (lookAtTarget.Value && (position - transform.position).sqrMagnitude > 0.01f)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(position - transform.position), maxLookAtRotationDelta.Value);
+        }
+        return TaskStatus.Running;
+    }
+    // Return targetPosition if targetTransform is null
+    private Vector3 Target()
+    {
+        if (targetObj == null || targetObj.Value == null)
+        {
+            //如果對象為null則設定目標位置為(0,0,0)並傳回錯誤訊息
+            Debug.LogError("目標為null，請設定目標");
+            return Vector3.zero;
+        }
+        return targetObj.Value.transform.position;
+    }
+    // Reset the public variables
+    public override void OnReset()
+    {
+        lookAtTarget = true;
+        SidePosValue = Vector3.zero;
+    }
+}
