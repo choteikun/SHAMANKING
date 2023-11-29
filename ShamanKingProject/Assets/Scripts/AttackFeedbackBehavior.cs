4cc46f48da997e47301b24858c99570ea5a4dbd3
darkcatneko <darkcat7126@gmail.com>
2023-11-29 23:06:13 +0800

    merge

--- ShamanKingProject/Assets/Scripts/AttackFeedbackBehavior.cs
+++ ShamanKingProject/Assets/Scripts/AttackFeedbackBehavior.cs
@@ -2,9 +2,13 @@ using Cysharp.Threading.Tasks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class AttackFeedbackBehavior : MonoBehaviour
 {
+    [SerializeField]  CinemachineVirtualCamera vCamera_;
+    [SerializeField] private CinemachineImpulseSource impulseSource_;
+    [SerializeField] float force_;
     [SerializeField] int delayFrame_ = 10;
     void Start()
     {
@@ -13,6 +17,7 @@ public class AttackFeedbackBehavior : MonoBehaviour
 
     async void attackHitTimeScale()
     {
+        impulseSource_.GenerateImpulse(force_);
         Time.timeScale = 0.15f;
         await UniTask.DelayFrame(delayFrame_);
         Time.timeScale = 1;
