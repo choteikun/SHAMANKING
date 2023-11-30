f11939831b39903fbb73133e2e7962fb27388139
草蛇 <blackzxz77@gmail.com>
2023-11-30 16:07:43 +0800

    merge

--- ShamanKingProject/Assets/Scripts/GrassSnakeScripts/Grasssnake_BT/Grasssnake_ExternalBehaviorTrees/Boss01_EBTs/FirstBossVariables.cs
+++ ShamanKingProject/Assets/Scripts/GrassSnakeScripts/Grasssnake_BT/Grasssnake_ExternalBehaviorTrees/Boss01_EBTs/FirstBossVariables.cs
@@ -18,18 +18,35 @@ public enum FirstBossState
 **************************************************/
 public class FirstBossVariables : MonoBehaviour
 {
+    [Tooltip("Boss狀態")]
     public FirstBossState FirstBossState;
     public int IntTypeStateOfFirstBoss { get { return intTypeStateOfFirstBoss_; } set { intTypeStateOfFirstBoss_ = value; } }
     private int intTypeStateOfFirstBoss_;
-    public bool UpdatePosTrigger { get { return updatePosTrigger_; } set { updatePosTrigger_ = value; } }
-    private bool updatePosTrigger_;
+    public int IntTypeOfBossFace { get { return intTypeOfBossFace_; } set { intTypeOfBossFace_ = value; } }
+    [SerializeField, Tooltip("檢查現在Boss的面具用的Int參數")]
+    private int intTypeOfBossFace_;
     public int FaceChangeCounter { get { return faceChangeCounter_; } set { faceChangeCounter_ = value; } }
+    [SerializeField, Tooltip("Boss換臉計時器")]
     private int faceChangeCounter_;
     public int MeleeAtkCounter { get { return meleeAtkCounter_; } set { meleeAtkCounter_ = value; } }
+    [SerializeField, Tooltip("Bossr近戰計時器")]
     private int meleeAtkCounter_;
     public int RangeAtkCounter { get { return rangedAtkCounter_; } set { rangedAtkCounter_ = value; } }
+    [SerializeField, Tooltip("Boss遠程計時器")]
     private int rangedAtkCounter_;
-   
+
+    public float FirstBossHp { get { return firstBossHp_; } set { firstBossHp_ = value; } }
+    [SerializeField, Tooltip("Boss血量")]
+    private float firstBossHp_;
+    public float FaceChangeProbability { get { return faceChangeProbability_; } set { faceChangeProbability_ = value; } }
+    [SerializeField, Tooltip("Boss換臉機率")]
+    private float faceChangeProbability_;
+    public float SkillProbability { get { return skillProbability_; } set { skillProbability_ = value; } }
+    [SerializeField, Tooltip("Boss技能機率")]
+    private float skillProbability_;
+
+    public bool UpdatePosTrigger { get { return updatePosTrigger_; } set { updatePosTrigger_ = value; } }
+    private bool updatePosTrigger_;
 
     void Start()
     {
@@ -47,6 +64,29 @@ public class FirstBossVariables : MonoBehaviour
                 break;
             case 3:
                 FirstBossState = FirstBossState.FirstBossState_RANGEDATKMODE;
+                break;
+            default:
+                break;
+        }
+        switch (FaceChangeCounter)
+        {
+            case 0:
+                FaceChangeProbability = 0;
+                break;
+            case 1:
+                FaceChangeProbability = 0;
+                break;
+            case 2:
+                FaceChangeProbability = 0;
+                break;
+            case 3:
+                FaceChangeProbability = 60;
+                break;
+            case 4:
+                FaceChangeProbability = 80;
+                break;
+            case 5:
+                FaceChangeProbability = 100;
                 break;
             default:
                 break;
