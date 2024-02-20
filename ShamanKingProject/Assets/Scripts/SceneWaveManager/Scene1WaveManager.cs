47d06f23ebe81b9402d86215356c6476dc0c34a0
darkcatneko <darkcat7126@gmail.com>
2024-02-20 18:00:44 +0800

    merge

--- ShamanKingProject/Assets/Scripts/SceneWaveManager/Scene1WaveManager.cs
+++ ShamanKingProject/Assets/Scripts/SceneWaveManager/Scene1WaveManager.cs
@@ -1,9 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using PixelCrushers.DialogueSystem;
-using static Unity.Burst.Intrinsics.X86.Avx;
-using Gamemanager;
+using UnityEngine;
+using DG.Tweening;
 
 public class Scene1WaveManager : MonoBehaviour
 {
@@ -14,26 +11,34 @@ public class Scene1WaveManager : MonoBehaviour
     [SerializeField] GhostSpawner[] wave2Spawners_;
     [SerializeField] GameObject BossSceneTransfer_;
     [SerializeField] int wave2Count_;
+    [SerializeField] GameObject wayPointPrefab_;
+    [SerializeField] GameObject wayPointObject_;
+    [SerializeField] GameObject wayPointEndPos_;
     void Start()
     {
         GameManager.Instance.MainGameEvent.SetSubscribe(GameManager.Instance.MainGameEvent.OnSystemCallWaveStart, cmd =>
         {
-        if (cmd.SceneName == "Scene1")
+            if (cmd.SceneName == "Scene1")
             {
-                waveWalls_[cmd.WaveID-1].SetActive(true);
-                switch(cmd.WaveID)
+                waveWalls_[cmd.WaveID].SetActive(true);
+                switch (cmd.WaveID)
                 {
+                    case 0:
+                        tutorial1Start();
+                        return;
                     case 2:
                         wave2Start();
                         return;
                 }
-            } });
+            }
+        });
         GameManager.Instance.MainGameEvent.SetSubscribe(GameManager.Instance.MainGameEvent.OnGhostIdentityCheck, cmd =>
         {
             if (cmd.GhostIdentityName == "Wave1_GhostEnemy")
             {
                 checkWave1GhostKilled();
-            } });
+            }
+        });
         GameManager.Instance.MainGameEvent.SetSubscribe(GameManager.Instance.MainGameEvent.OnGhostIdentityCheck, cmd =>
         {
             if (cmd.GhostIdentityName == "Wave2_GhostEnemy")
@@ -42,14 +47,14 @@ public class Scene1WaveManager : MonoBehaviour
             }
         });
     }
-    
+
     void Update()
     {
-        
+
     }
     void wave2Start()
     {
-        foreach (var spawners in wave2Spawners_) 
+        foreach (var spawners in wave2Spawners_)
         {
             spawners.SpawnGhost();
         }
@@ -57,7 +62,7 @@ public class Scene1WaveManager : MonoBehaviour
     void checkWave1GhostKilled()
     {
         wave1GhostKilled_++;
-        if (wave1GhostKilled_>=1)
+        if (wave1GhostKilled_ >= 1)
         {
             waveWalls_[0].SetActive(false);
             DialogueManager.StartConversation("chapter 1_1_2");
@@ -74,8 +79,14 @@ public class Scene1WaveManager : MonoBehaviour
             //DialogueManager.StartConversation("chapter1_2_2");
         }
     }
-    void checkWave3GhostKilled() 
+    void checkWave3GhostKilled()
     {
 
+    }
+    void tutorial1Start()
+    {
+        var wayPointStartPosition = GameManager.Instance.MainGameMediator.RealTimePlayerData.PlayerGameObject.transform.position;
+        wayPointObject_ = Instantiate(wayPointPrefab_, wayPointStartPosition, Quaternion.identity);
+        wayPointObject_.transform.DOMove(wayPointEndPos_.transform.position, 0.25f);
     }
 }
