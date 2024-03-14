1e8be5a952b1cc9d33f3cd7860c377c1ba2ffd73
yuan0717zero <4a9k0017@stust.edu.tw>
2024-03-14 14:45:28 +0800

    Punishment修正

--- ShamanKingProject/Assets/Scripts/Boss1Scripts/PunishmentMachine.cs
+++ ShamanKingProject/Assets/Scripts/Boss1Scripts/PunishmentMachine.cs
@@ -8,8 +8,8 @@ public class PunishmentMachine : MonoBehaviour
     [SerializeField] GameObject centerHint_;
     [SerializeField] GameObject outerHitBox_;
     [SerializeField] GameObject centerHitBox_;
-    [SerializeField] GameObject InnerJudgement_;
-    [SerializeField] GameObject OuterJudgement_;
+    [SerializeField] GameObject innerJudgement_;
+    [SerializeField] GameObject outerJudgement_;
 
     [SerializeField] int centerHintFrame_;
     [SerializeField] int outerHintFrame_;
@@ -22,23 +22,23 @@ public class PunishmentMachine : MonoBehaviour
     async void punishmentAttack()
     {
         centerHint_.SetActive(true);
-        InnerJudgement_.SetActive(true);
+        innerJudgement_.SetActive(true);
         await UniTask.DelayFrame(centerHintFrame_);
         centerHint_.SetActive(false);
         centerHitBox_.SetActive(true);
         GameManager.Instance.MainGameEvent.Send(new GameCallSoundEffectGenerate() { SoundEffectID = 29 });
         await UniTask.DelayFrame(3);
         Destroy(centerHitBox_);
-        Destroy(InnerJudgement_);
+        Destroy(innerJudgement_);
         outerHint_.SetActive(true);
-        OuterJudgement_.SetActive(true);
+        outerJudgement_.SetActive(true);
         await UniTask.DelayFrame(outerHintFrame_);
         outerHint_.SetActive(false);
         outerHitBox_.SetActive(true);
         GameManager.Instance.MainGameEvent.Send(new GameCallSoundEffectGenerate() { SoundEffectID = 29 });
         await UniTask.DelayFrame(3);
         Destroy(outerHitBox_);
-        Destroy(OuterJudgement_);
+        Destroy(outerJudgement_);
         GameManager.Instance.MainGameEvent.Send(new BossPunishmentAttackEndCommand());
         await UniTask.DelayFrame(3);
         Destroy(gameObject);
