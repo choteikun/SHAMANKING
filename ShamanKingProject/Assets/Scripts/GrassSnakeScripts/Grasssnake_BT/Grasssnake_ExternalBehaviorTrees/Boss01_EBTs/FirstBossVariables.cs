8d787789861e92cd1e11ad0340d0c356dce75384
草蛇 <blackzxz77@gmail.com>
2024-03-14 14:46:04 +0800

    merge

--- ShamanKingProject/Assets/Scripts/GrassSnakeScripts/Grasssnake_BT/Grasssnake_ExternalBehaviorTrees/Boss01_EBTs/FirstBossVariables.cs
+++ ShamanKingProject/Assets/Scripts/GrassSnakeScripts/Grasssnake_BT/Grasssnake_ExternalBehaviorTrees/Boss01_EBTs/FirstBossVariables.cs
@@ -308,6 +308,7 @@ public class FirstBossVariables : MonoBehaviour
     }
     void explosionJudgmentEnd()
     {
+        Debug.Log("關閉疲倦觸發器");
         TirednessTrigger = false;
     }
     public void OnUpdateRootMotion(Animator anim)
