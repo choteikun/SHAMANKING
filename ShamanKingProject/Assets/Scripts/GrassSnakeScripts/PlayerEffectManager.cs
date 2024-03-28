70f94d8e75416fd518f259053d7ebebe242a28de
choteikun <blackzxz77@gmail.com>
2024-03-28 18:19:37 +0800

    蓄力特效實裝

--- ShamanKingProject/Assets/Scripts/GrassSnakeScripts/PlayerEffectManager.cs
+++ ShamanKingProject/Assets/Scripts/GrassSnakeScripts/PlayerEffectManager.cs
@@ -1,3 +1,5 @@
+using Gamemanager;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,43 +8,60 @@ using UnityEngine.VFX;
 public class PlayerEffectManager : MonoBehaviour
 {
     [SerializeField]
-    GameObject playerEffectObjsRoot_;
+    GameObject playerChargeEffectObjsRoot_;
     [SerializeField]
-    VisualEffect[] playerEffects_;
+    VisualEffect[] playerChargeEffects_;
 
     void Start()
     {
-        GameManager.Instance.MainGameEvent.SetSubscribe(GameManager.Instance.MainGameEvent.OnPlayerHeavyAttack, cmd => {  });
-        GameManager.Instance.MainGameEvent.SetSubscribe(GameManager.Instance.MainGameEvent.OnPlayerHeavyAttackChargeFinish, cmd => { });
-        if (playerEffectObjsRoot_ == null)
+        GameManager.Instance.MainGameEvent.SetSubscribe(GameManager.Instance.MainGameEvent.OnPlayerChargeSwitch, onPlayerChargeSwitch);
+        GameManager.Instance.MainGameEvent.SetSubscribe(GameManager.Instance.MainGameEvent.OnPlayerHeavyAttackChargeFinish, cmd => {playerHeavyAttackEffectComplete(); });
+        
+        if (playerChargeEffectObjsRoot_ == null)
         {
-            playerEffectObjsRoot_ = GameObject.Find("PlayerEffectObjs").gameObject;
-
-            playerEffects_ = new VisualEffect[playerEffectObjsRoot_.transform.childCount];
-
-            for (int i = 1; i <= playerEffectObjsRoot_.transform.childCount; i++)
+            try
             {
-                playerEffects_[i - 1] = playerEffectObjsRoot_.transform.GetChild(i - 1).GetComponent<VisualEffect>();
-                playerEffects_[i - 1].Stop();
+                playerChargeEffectObjsRoot_ = GameObject.Find("PlayerChargeEffectObjs").gameObject;
+            }
+            catch (NullReferenceException)
+            {
+                Debug.LogError("可能是蓄力特效遺失，請將Assets/GameEffect/Girl_VFX/Charge/PlayerChargeEffectObjs.prefab 裝在PlayerCharacter裡的forearm.L(左手腕)上");
+                throw;
+            }
+
+            playerChargeEffects_ = new VisualEffect[playerChargeEffectObjsRoot_.transform.childCount];
+
+            for (int i = 1; i <= playerChargeEffectObjsRoot_.transform.childCount; i++)
+            {
+                playerChargeEffects_[i - 1] = playerChargeEffectObjsRoot_.transform.GetChild(i - 1).GetComponent<VisualEffect>();
+                playerChargeEffects_[i - 1].Stop();
             }
         }
     }
-    void playerHeavyAttackEffectSpawn()
+    void onPlayerChargeSwitch(PlayerChargeSwitchCommand playerChargeSwitchCommand)
     {
-        playerEffects_[0].Play();
-
-    }
-    void playerHeavyAttackEffectSuccess()
-    {
-
+        if (playerChargeSwitchCommand.Switch)
+        {
+            //蓄力特效播放
+            playerChargeEffects_[0].Play();
+        }
+        else
+        {
+            ////蓄力特效結束
+            playerChargeEffects_[0].Stop();
+            playerChargeEffects_[2].Stop();
+        }
     }
     void playerHeavyAttackEffectComplete()
     {
-
+        //蓄力完成瞬間的提示特效播放
+        playerChargeEffects_[1].Play();
+        //蓄力完成後的特效播放
+        playerChargeEffects_[2].Play();
     }
 
     void Update()
     {
-        
+
     }
 }
