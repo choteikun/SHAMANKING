a3bd179667c568685fd51b7ae31d503c29f28c0b
草蛇 <blackzxz77@gmail.com>
2024-04-09 03:14:52 +0800

    音效調整/boss阻擋牆修正

--- ShamanKingProject/Assets/Scripts/Boss1Scripts/FireballProjectileBehavior.cs
+++ ShamanKingProject/Assets/Scripts/Boss1Scripts/FireballProjectileBehavior.cs
@@ -42,6 +42,7 @@ public class FireballProjectileBehavior : MonoBehaviour
             var explodeEffect = GameContainer.Get<DataManager>().GetDataByID<GameEffectTemplete>(10).PrefabPath;
             var explodeObject = Instantiate(explodeEffect, other.ClosestPoint(this.gameObject.transform.position), Quaternion.identity);
             GameManager.Instance.MainGameEvent.Send(new GameCallSoundEffectGenerate() {SoundEffectID = 202 });
+            Destroy(gameObject);
         }
     }
 }
