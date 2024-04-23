a1b593b3e29874088693748bd00c567d45b90473
darkcatneko <darkcat7126@gmail.com>
2024-04-23 18:20:36 +0800

    Change UI

--- ShamanKingProject/Assets/Scripts/UIController/SpiritUIBehavior.cs
+++ ShamanKingProject/Assets/Scripts/UIController/SpiritUIBehavior.cs
@@ -1,37 +1,56 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SpiritUIBehavior : MonoBehaviour
 {
-    [SerializeField] Sprite emptySpirit_;
-    [SerializeField] Sprite fullSpirit_;
     [SerializeField] Image[] spiritImages_;
 
     private void Start()
     {
-        SpiritUIInit();
+        GameManager.Instance.UIGameEvent.SetSubscribe(GameManager.Instance.UIGameEvent.OnSoulGageUpdate, cmd => { onSpiritUpdate(GameManager.Instance.MainGameMediator.RealTimePlayerData.GhostSoulGageCurrentAmount); });
     }
-    public void SpiritUIInit()
+    
+    void onSpiritUpdate(float gageAmount)
     {
-        //GameManager.Instance.UIGameEvent.SetSubscribe(GameManager.Instance.UIGameEvent.OnSpiritUpdate, cmd => { onSpiritUpdate(); });
-    }
-
-    void onSpiritUpdate()
-    {
-        //ChangeSpiritImage(GameManager.Instance.MainGameMediator.RealTimePlayerData.GhostNowEatAmount);
-    }
-    public void ChangeSpiritImage(int nowSpiritCount)
-    {
-        for (int i = 0; i < spiritImages_.Length; i++) 
+        if (gageAmount>300)
         {
-            if (i<nowSpiritCount)
+            var amount4 = (gageAmount-300)/100f;
+            spiritImages_[3].DOFillAmount(amount4, 0.25f);
+            spiritImages_[2].DOFillAmount(1, 0.25f);
+            spiritImages_[1].DOFillAmount(1, 0.25f);
+            spiritImages_[0].DOFillAmount(1, 0.25f);
+        }
+        else
+        {
+            spiritImages_[3].DOFillAmount(0, 0.25f);
+            if (gageAmount > 200)
             {
-                spiritImages_[i].sprite = fullSpirit_;
+                var amount3 = (gageAmount - 200) / 100f;
+                spiritImages_[2].DOFillAmount(amount3, 0.25f);
+                spiritImages_[1].DOFillAmount(1, 0.25f);
+                spiritImages_[0].DOFillAmount(1, 0.25f);
             }
             else
             {
-                spiritImages_[i].sprite = emptySpirit_;
+                spiritImages_[2].DOFillAmount(0, 0.25f);
+                if (gageAmount>100)
+                {
+                    var amount2 = (gageAmount - 100) / 100f;
+                    spiritImages_[1].DOFillAmount(amount2, 0.25f);
+                    spiritImages_[0].DOFillAmount(1, 0.25f);
+                }
+                else
+                {
+                    spiritImages_[1].DOFillAmount(0, 0.25f);
+                    var amount = gageAmount / 100f;
+                    spiritImages_[0].DOFillAmount(amount, 0.25f);
+                }
             }
         }
+        
+        
+       
     }
+   
 }
