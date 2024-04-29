e03648b69b028fde75db6cc140a0dd64a4f46ab2
darkcat7126 <darkcat7126@gmail.com>
2024-04-29 23:28:32 +0800

    elite cam fix

---
+++ ShamanKingProject/Assets/Scripts/SceneWaveManager/WaveCamTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamemanager;
+
+public class WaveCamTrigger : MonoBehaviour
+{
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    CallFirstSceneCameraTransfer(0);
+    //}
+    //public void CallFirstSceneCameraTransfer(float firstSceneCameraTransfer)
+    //{
+    //    GameManager.Instance.MainGameEvent.Send(new SystemCallFirstSceneCameraTransferCommand() { CameraId = firstSceneCameraTransfer });
+    //    GameManager.Instance.MainGameEvent.Send(new SystemStopGuardingCommand());
+    //}
+}
